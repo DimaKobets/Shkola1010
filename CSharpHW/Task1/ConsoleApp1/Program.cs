ae572a8185aeb9558bd274634bb718abccb7393d
Dima <3fid96@gmail.com>
2018-06-02 23:10:53 +0300

    update

---
+++ CSharpHW/Task1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Меня зовут Дима");
+            Console.ReadKey();
+        }
+    }
+}
