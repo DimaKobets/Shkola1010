11d40923481a770a86bc532c359c87b33760d355
Dima <3fid96@gmail.com>
2018-06-02 23:02:48 +0300

    Add homework 2

---
+++ CSharpHW/Task2/Project2/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ComboBox.Items.Add("byte");
+            ComboBox.Items.Add("sbyte");
+            ComboBox.Items.Add("short");
+            ComboBox.Items.Add("ushort");
+            ComboBox.Items.Add("int");
+            ComboBox.Items.Add("uint");
+            ComboBox.Items.Add("long");
+            ComboBox.Items.Add("ulong");
+
+        }
+
+        
+      
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string comboBoxValue = ComboBox.SelectedItem.ToString();
+
+            switch (comboBoxValue)
+            {
+                case "byte":
+                    ShowMassage("byte", "8", "0", "255");
+                    break;
+                case "sbyte":
+                    ShowMassage("sbyte", "8", "-128", "127");
+                    break;
+                case "short":
+                    ShowMassage("short", "16", "-32768", "32767");
+                    break;
+                case "ushort":
+                    ShowMassage("ushort", "16", "0", "65535");
+                    break;
+                case "int":
+                    ShowMassage("int", "32", "-2147483648", "2147483647");
+                    break;
+                case "uint":
+                    ShowMassage("uint", "32", "0", "4294967295");
+                    break;
+                case "long":
+                    ShowMassage("long", "64", "-9223372036854775808", "9223372036854775807");
+                    break;
+                case "ulong":
+                    ShowMassage("ulong", "64", "0", "18446744073709551615");
+                    break;             
+                    
+            }
+
+        }
+
+        private void ShowMassage(string tipe, string bitDepth, string minValue, string maxValue)
+        {
+            massage.Text = "Числовой тип " + tipe + " имеет разрядность " + bitDepth +
+               " бит и его значения лежат в диапазоне от " + minValue + " до " + maxValue;
+        }
+
+    }
+}
---
+++ CSharpHW/Task2/Project3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выражение и нажмите Enter");
+            String expression = Console.ReadLine();
+            Calculate(expression);
+            Console.ReadKey();            
+        }
+
+        static void Calculate(string str)
+        {            
+            string[] splitedString = SplitString(str);
+            double[] operands = SringToDouble(splitedString);            
+            
+            if (str.Contains('+'))
+            {
+                Console.WriteLine(operands[0] + operands[1]);
+            }
+            else if (str.Contains('-'))
+            {
+                Console.WriteLine(operands[0] - operands[1]);
+            }
+            else if (str.Contains('*'))
+            {
+                Console.WriteLine(operands[0] * operands[1]);
+            }
+            else if (str.Contains('/'))
+            {
+                Console.WriteLine(operands[0] / operands[1]);
+            } 
+
+        }
+
+
+        static string[] SplitString(string str)
+        {
+            string[] split = str.Split(new char[] { '+', '-', '*', '/' });
+            return split;
+        }
+
+        static double[] SringToDouble(string[] splitedString)
+        {
+            double[] operand = new double[2]; 
+            try
+            {
+                operand[0] = Convert.ToDouble(splitedString[0]);
+                operand[1] = Convert.ToDouble(splitedString[1]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено неправильное выражение");                
+            }
+            return operand;
+        }
+
+    }
+}
