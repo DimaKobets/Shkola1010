2501b9bd0651ed7821733dd64db7c42886501b53
Dima <3fid96@gmail.com>
2018-06-05 00:47:39 +0300

    Add HW1

---
+++ Frontend/Task1/user.service.js
@@ -0,0 +1,8 @@
+var age = window.prompt("How old are you?");
+if (age >= "18"){
+    alert("You have access!")
+}
+else {
+    location.href = 'https://youtu.be/OK03o3BHafk' ;
+    Location.assign(location.href);
+}
